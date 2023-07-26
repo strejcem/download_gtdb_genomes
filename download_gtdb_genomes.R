ef553300d15af7a85e7cf969470f9d134b95fb6f
strejcem <strejcem@vscht.cz>
2023-07-26 19:21:39 +0200

    added functions: - pick number of random genomes - can pick genomes from taxonomic sub-levels - can generate only accession lists

--- download_gtdb_genomes.R
+++ download_gtdb_genomes.R
@@ -1,37 +1,57 @@
 #!/usr/bin/env Rscript
 
+
 library(argparser, quietly = TRUE)
 
-p <- arg_parser("Version 1.21, by Michal Strejcek @ UCT Prague
-                Downloads NCBI assemblies from RefSeq or GenBank based on GTDB taxonomy labels.
-		
-                At least one of Arc/Bac taxonomy/metadata file needs to be specified.
-                Taxonomy/metadata files can be accessed at https://data.gtdb.ecogenomic.org/releases/latest/
-                If metadata file is used, you can filter for gtdb_representative genomes only, i.e. genomes used in GTDB reference tree.
 
-		Depends on:
-	       		'conda isntall r-tidyverse r-argparser ncbi-genome-download'
-	                'wget https://data.gtdb.ecogenomic.org/releases/latest/ar53_metadata.tar.gz'
-	                'wget https://data.gtdb.ecogenomic.org/releases/latest/bac120_metadata.tar.gz'
-			'wget https://data.gtdb.ecogenomic.org/releases/latest/VERSION.txt'
-		")
+p <- arg_parser(hide.opts = TRUE,
+  "Version 1.3, by Michal Strejcek @ UCT Prague
+  Downloads NCBI assemblies from RefSeq or GenBank based on GTDB taxonomy labels.
+
+  At least one of Arc/Bac taxonomy/metadata file needs to be specified.
+  Taxonomy/metadata files can be accessed at https://data.gtdb.ecogenomic.org/releases/latest/
+  If metadata file is used, you can filter for gtdb_representative genomes only, i.e. genomes used in GTDB reference tree.
+
+  Depends on:
+  'conda install r-tidyverse r-argparser ncbi-genome-download'
+  'wget https://data.gtdb.ecogenomic.org/releases/latest/ar53_metadata.tar.gz'
+  'wget https://data.gtdb.ecogenomic.org/releases/latest/bac120_metadata.tar.gz'
+  'wget https://data.gtdb.ecogenomic.org/releases/latest/VERSION.txt'
+"
+)
 p <- add_argument(p, "--arc_tax", help = "File location for 'ar122_taxonomy_rXX.tsv(.gz)'", default = NA)
 p <- add_argument(p, "--bac_tax", help = "File location for 'bac120_taxonomy_rXX.tsv(.gz)'", default = NA)
 p <- add_argument(p, "--arc_meta", help = "File location for 'ar122_metadata_rXX.tsv(.gz)'", default = NA)
 p <- add_argument(p, "--bac_meta", help = "File location for 'bac120_metadata_rXX.tsv(.gz)'", default = NA)
 p <- add_argument(p, "--out_dir", help = "Output directory", default = NA)
-p <- add_argument(p, "--tag_GTDB", help = "Comma-separated list of gtdb taxonomy
+p <- add_argument(p, "--gtdb_tag", help = "Comma-separated list of gtdb taxonomy
                   e.g. 'p__Desantisbacteria,g__Escherichia,g__Bacilli'", default = NA)
-p <- add_argument(p, "--representative", help = "Download only GTDB representative genomes.", flag = TRUE)
-p <- add_argument(p, "--keep_intermediate", help = "Keep intermediate files.", flag = TRUE)
+p <- add_argument(p, "-F", help = "[fasta|protein-fasta|...] Argument for 'ncbi-genome-download',
+                  see its help for more options.", default = "fasta")
+p <- add_argument(p, "--representatives", help = "[TRUE|FALSE] Download only GTDB representative
+                  or all genomes.", default = TRUE)
+p <- add_argument(p, "--keep_intermediate", help = "Keep intermediate files. Includes accession lists
+                  and donwloaded files", defau = TRUE)
+p <- add_argument(p, "--num_genomes", help = "[integer] Pick specific number of random genomes
+                  per the '--gtdb_tag' entry.", default = NA, type = "integer")
+p <- add_argument(p, "--tax_level", help = "One of [phylum, class, order, family,
+                  genus, species]. To be used with '--num_genomes'. Move the 
+                  '--num_genomes' selection to a specified taxnomic rank within the '--gtdb_tag'
+                  entries. Eg. '--gtdb_tag f__Pseudomonadaceae,c__Heimdallarchaeia --num_genomes 3
+                  --tax_level genus' will download three random genomes of all genera within
+                  Pseudomonadaceae family and  Heimdallarchaeia class.", default = NA)
+p <- add_argument(p, "--just_list", help = "Creates the accession list but no download.", flag = TRUE)
+p <- add_argument(p, "--seed", help = "Seed number for reproducible random sampling", type = "integer", default = NA)
+
 argv <- parse_args(p)
 
 suppressPackageStartupMessages(library(tidyverse))
 
 out_dir <- file.path(argv$out_dir)
 
-if(is.na(argv$tag_GTDB)) {
-  stop("Taxonomy tags must be specified!")
+if(is.na(argv$gtdb_tag)) {
+  stop("Taxonomy tags must be specified!
+       Use '-h' for help!")
 }
 
 if(is.na(out_dir)) {
@@ -53,10 +73,46 @@ if(all(at, am) | all(at, bm) | all(bt, bm) | all(bt, am)) {
   stop("Please use either taxonomy or metadata file(s) only!")
 }
 
-to_pick <- str_split(argv$tag_GTDB, pattern = ",")[[1]]
+if (!is.na(argv$num_genomes) & 
+    !(argv$num_genomes) > 0 
+    ) {
+  stop("--num_genomes must be > 0!")
+}
+
+
+if (!is.na(argv$num_genomes) & 
+    (!as.integer(argv$num_genomes) > 0 | !is.integer(argv$num_genomes))
+) {
+  stop("--num_genomes must be > 0!")
+}
+
+if (!is.na(argv$tax_level) &
+    !argv$tax_level %in% c("Phylum", "Class", "Order", "Family", "Genus", "Species")) {
+  stop("'--tax_level' must be one of [Phylum|Class|Order|Family|Genus|Species]")
+}
+
+if (!is.na(argv$tax_level) &
+    is.na(argv$num_genomes)) {
+  stop("When using '--tax_level' you must also specify '--num_genomes'!")
+}
+
+download_format <- str_split_1(argv$F, pattern = ",")
+if (!all(download_format %in% c("genbank", "fasta", "rm", "features", "gff", "protein-fasta", "genpept","wgs",
+  "cds-fasta", "rna-fna", "rna-fasta", "assembly-report", "assembly-stats", "all"))) {
+  stop("-F must be one of ['genbank', 'fasta', 'rm', 'features', 'gff', 'protein-fasta', 
+       'genpept','wgs', 'cds-fasta', 'rna-fna', 'rna-fasta', 'assembly-report',
+       'assembly-stats', 'all']!")
+}
+
+if (!is.na(argv$seed)) {
+  set.seed(argv$seed)
+}
+
+to_pick <- str_split_1(argv$gtdb_tag, pattern = ",")
 
 #prepare GTDB taxonomy database
 if(am) {
+  message("Reading metadata for Archaea.")
   gtdb_arc <-read_tsv(file.path(argv$arc_meta),
                       col_types = cols_only(
                         accession = 'c',
@@ -76,7 +132,9 @@ if(am) {
       ),
       sep = ";"
     )
+  message("Done.")
 } else if(at) {
+  message("Reading taxonomy for Archaea.")
   gtdb_arc <-
     read_tsv(file.path(argv$arc_tax),
              col_names = c("accession", "tax"),
@@ -94,11 +152,14 @@ if(am) {
       ),
       sep = ";"
     )
+  message("Done.")
 } else {
+  message("Skipping Archaea!")
   gtdb_arc <- NULL
 }
 
 if(bm) {
+  message("Reading metadata for Bacteria")
   gtdb_bac <-read_tsv(file.path(argv$bac_meta),
                       col_types = cols_only(
                         accession = 'c',
@@ -118,7 +179,9 @@ if(bm) {
       ),
       sep = ";"
     )
+  message("Done.")
 } else if(bt) {
+  message("Reading taxonomy for Bacteria.")
   gtdb_bac <-
     read_tsv(file.path(argv$bac_tax),
              col_names = c("accession", "tax"),
@@ -136,7 +199,9 @@ if(bm) {
       ),
       sep = ";"
     )
+  message("Done.")
 } else {
+  message("Skipping Bacteria!")
   gtdb_bac <- NULL
 }
 
@@ -147,11 +212,42 @@ gtdb_tax <- bind_rows(gtdb_arc, gtdb_bac) %>%
     accession = str_remove(accession, "^(GB|RS)_")
   )
 
+rm(gtdb_arc, gtdb_bac)
+
+message("Selecting accessions...")
 tax_selection <- gtdb_tax %>%
-  {if(argv$representative) filter(., gtdb_representative) else .} %>%
-  select(accession, section, Domain, Phylum, Class, Order, Family, Genus, Species) %>% 
+  {
+    if (argv$representatives) {
+      filter(., gtdb_representative)
+    } else {
+      .
+    }   
+  } %>%
+  select(accession,
+         section,
+         Domain,
+         Phylum,
+         Class,
+         Order,
+         Family,
+         Genus,
+         Species) %>%
   pivot_longer(names_to = "taxonomy", cols = -c(accession, section)) %>%
   filter(value %in% to_pick) %>%
+  distinct(accession) %>% 
+  left_join(gtdb_tax, by = "accession") %>% 
+  {
+    if (!is.na(argv$tax_level)) {
+      select(., accession, section, matches(argv$tax_level)) %>% 
+      group_by(across(argv$tax_level)) %>% 
+      slice_sample(n = argv$num_genomes) %>% 
+      ungroup()
+    } else if (!is.na(argv$num_genomes)) { 
+        slice_sample(., n = argv$num_genomes)
+    } else {
+      .
+    }
+  } %>% 
   distinct(accession, section)
 
 num_acc <- nrow(tax_selection)
@@ -176,72 +272,99 @@ dir.create(tmp_dir)
 
 #download GenBank
 if (GB > 0) {
-  message("Downloading ", GB, " genomes from GenBank")
+
   GB_file <- file.path(tmp_dir, "accession_to_download_GB.txt")
+  
   tax_selection %>%
     filter(section == "GB") %>%
     select(accession) %>%
     write_tsv(file = GB_file, col_names = FALSE)
   
-  ncbi_genome_download_arguments <- str_c(sep = " ",
-    "-A", GB_file, "-s genbank -F fasta -p 4 -o", tmp_dir, "--no-cache", "--debug", NCBI_groups
-  )
-  system2("ncbi-genome-download", args = ncbi_genome_download_arguments)
+  if (!argv$just_list) {
+    message("Downloading ", GB, " genomes from GenBank") 
+    ncbi_genome_download_arguments <- str_c(sep = " ",
+      "-A", GB_file, "-s genbank -F", argv$F, "-p 4 -o", tmp_dir, "--no-cache", NCBI_groups
+    )
+    system2("ncbi-genome-download", args = ncbi_genome_download_arguments)
+  }
 }
 
 #download RefSeq
 if (RS > 0) {
-  message("Downloading ", RS, " genomes from RefSeq")
+  
   RS_file <- file.path(tmp_dir, "accession_to_download_RS.txt")
   tax_selection %>%
     filter(section == "RS") %>%
     select(accession) %>%
     write_tsv(file = RS_file, col_names = FALSE)
   
-  ncbi_genome_download_arguments <- str_c(sep = " ",
-    "-A", RS_file, "-s refseq -F fasta -p 4 -o", tmp_dir, "--no-cache", "--debug", NCBI_groups
+  if (!argv$just_list) {
+    message("Downloading ", RS, " genomes from RefSeq")
+    ncbi_genome_download_arguments <- str_c(sep = " ",
+      "-A", RS_file, "-s refseq -F", argv$F, "-p 4 -o", tmp_dir, "--no-cache", NCBI_groups
+    )
+    system2("ncbi-genome-download", args = ncbi_genome_download_arguments)
+  }
+}
+
+dir.create(out_dir, recursive = TRUE)
+if (argv$just_list) {
+  invisible(
+    file.copy(
+      from = GB_file,
+      to = file.path(out_dir, "accession_to_download_GB.txt")
+    ))
+  invisible(
+    file.copy(
+      from = RS_file,
+      to = file.path(out_dir, "accession_to_download_RS.txt")
+    )
+  )
+  unlink(tmp_dir, recursive = TRUE)
+  message("All done!")
+} else {
+  #move genome files to a new directory
+  
+  ncbi_genomes <-
+    list.files(
+      tmp_dir,
+      pattern = "\\.gz$",
+      full.names = TRUE,
+      recursive = TRUE
+    )
+  
+  new_names <- ncbi_genomes %>% 
+    basename() %>% 
+    str_replace("_[:alpha:]+[:digit:]+v[:digit:]+_", "_")
+  
+  invisible(
+    file.copy(
+      from = file.path(ncbi_genomes),
+      to = file.path(out_dir, new_names),
+      overwrite = FALSE
+    )
   )
   
-  system2("ncbi-genome-download", args = ncbi_genome_download_arguments)
-}
-
-#move genome files to a new directory
-dir.create(out_dir, recursive = TRUE)
-ncbi_genomes <-
-  list.files(
-    tmp_dir,
-    pattern = "*fna.gz",
-    full.names = TRUE,
-    recursive = TRUE
-  )
-only_accession <-
-  str_extract(basename(ncbi_genomes), "(GCA|GCF)_[:digit:]+\\.[:digit:]+")
-new_names <- str_c(only_accession, ".fna.gz")
-file.copy(
-  from = file.path(ncbi_genomes),
-  to = file.path(out_dir, new_names),
-  overwrite = TRUE
-)
-
-if(!argv$keep_intermediate) {
-  unlink(tmp_dir, recursive = TRUE)
-}
-
-#check that everything went ok
-downloaded_files <-
-  list.files(out_dir, pattern = "*fna.gz", full.names = FALSE)
-downloaded_accession <- str_remove(downloaded_files, ".fna.gz")
-missing_genomes <- tax_selection %>%
-  filter(!accession %in% downloaded_accession) %>% 
-  pull(accession)
-
-
-if (length(missing_genomes) == 0) {
-  message("All genomes succesfully downloaded!")
-} else {
-  message("Missing these genomes:")
-  gtdb_tax %>% 
-    filter(accession %in% missing_genomes) %>% 
-    select(accession, Domain, Phylum, Genus) %>% 
-    print(n=20)
-}
+  if(!argv$keep_intermediate) {
+    unlink(tmp_dir, recursive = TRUE)
+  }
+  
+  #check that everything went ok
+  downloaded_files <-
+    list.files(out_dir, pattern = "*.gz", full.names = FALSE)
+  downloaded_accession <- str_extract(basename(downloaded_files), "(GCA|GCF)_[:digit:]+\\.[:digit:]+")
+  missing_genomes <- tax_selection %>%
+    filter(!accession %in% downloaded_accession) %>% 
+    pull(accession)
+  
+  
+  if (length(missing_genomes) == 0) {
+    message("***!!! All genomes succesfully downloaded !!!***")
+  } else {
+    message("Missing these genomes:")
+    gtdb_tax %>% 
+      filter(accession %in% missing_genomes) %>% 
+      select(accession, Domain, Phylum, Genus) %>% 
+      print(n=20)
+  }
+} 
